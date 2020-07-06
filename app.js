da46469d00580464059672487496dca37aeb5c70
memset0 <memset0@outlook.com>
2020-07-06 23:27:57 +0800

    wssb

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ const config = YAML.parse(fs.readFileSync(path.join(__dirname, './config.yml')).
 app.get('/', function (req, res) { res.send(fs.readFileSync(path.join(__dirname, './index.html')).toString()); });
 app.get('/script.js', function (req, res) { res.send(fs.readFileSync(path.join(__dirname, './script.js')).toString().replace('WebSocketUrl', config.ws)); });
 
+let clients = [];
+
+function updateAllArticle() { clients.forEach(ws => ws.send(JSON.stringify({ type: 'update_article', article: api.getArticle() }))); }
+function updateAllHistory() { clients.forEach(ws => ws.send(JSON.stringify({ type: 'update_article', article: api.getArticle() }))); }
+
 app.ws('/', function (ws, req) {
+	clients.push(ws);
 	ws.on('message', function (event) {
 		try {
 			event = JSON.parse(event);
@@ -22,23 +28,21 @@ app.ws('/', function (ws, req) {
 			({
 				push_article: function (ws, user, data) {
 					api.pushArticle(user, data);
-					ws.send(JSON.stringify({ type: 'update_article', article: api.getArticle() }));
+					updateAllArticle();
 				},
 				pop_article: function (ws, user) {
 					api.popArticle(user);
-					ws.send(JSON.stringify({ type: 'update_article', article: api.getArticle() }));
+					updateAllArticle();
 				},
 				end_article: function (ws, user) {
 					api.endArticle(user);
-					ws.send(JSON.stringify({ type: 'update_article', article: api.getArticle() }));
-					ws.send(JSON.stringify({ type: 'update_history', history: api.getHistory() }));
+					updateAllArticle(), updateAllHistory();
 				},
 				login: function (ws, user) {
 					console.log(user);
 					if (user && user.permission && user.permission.allowEdit) {
 						ws.send(JSON.stringify({ type: 'login' }));
-						ws.send(JSON.stringify({ type: 'update_article', article: api.getArticle() }));
-						ws.send(JSON.stringify({ type: 'update_history', history: api.getHistory() }));
+						updateAllArticle(), updateAllHistory();
 					} else {
 						ws.send(JSON.stringify({ type: 'error', message: '密码错误' }));
 					}
@@ -49,6 +53,9 @@ app.ws('/', function (ws, req) {
 			ws.send(JSON.stringify({ type: 'error', message: e }));
 		}
 	});
+	ws.onclose=function(){
+		clients = clients.filter(client => client !== ws);
+	};
 });
 
 app.listen(process.env.PORT || 4514);
